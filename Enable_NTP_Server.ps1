28fb141578c3016d88b38e626908d9121cfaf662
Twikki <46316044+Twikki@users.noreply.github.com>
2019-09-03 10:10:11 +0200

    Enables NTP server in Windows and sets configuration
    - Enables NTP server

---
+++ Enable_NTP_Server.ps1
@@ -0,0 +1,27 @@
+﻿# Enables NTP server on Windows server, allowing other devices to query time from this server.
+
+Set-Location HKLM:\SYSTEM\CurrentControlSet\services\W32Time\TimeProviders\NtpServer
+Set-ItemProperty . Enabled "1"
+
+
+w32tm /config /manualpeerlist:0.dk.pool.ntp.org,1.dk.pool.ntp.org /syncfromflags:MANUAL /reliable:yes /update
+
+
+# Set's the NTP Server to Peers
+Set-Location HKLM:\SYSTEM\CurrentControlSet\services\W32Time\Parameters
+Set-ItemProperty . NtpServer "Peers"
+
+# Set's the NTP Server to Peers
+Set-Location HKLM:\SYSTEM\CurrentControlSet\services\W32Time\Parameters
+Set-ItemProperty . Type "NTP"
+
+# Set's the NTP Server to Peers
+Set-Location HKLM:\SYSTEM\CurrentControlSet\services\W32Time\Config
+Set-ItemProperty . AnnounceFlags "5"
+
+# Set's the NTP Server to Peers
+Set-Location HKLM:\SYSTEM\CurrentControlSet\services\W32Time\TimeProviders\NtpClient
+Set-ItemProperty . SpecialPollInterval "3600"
+
+Stop-Service w32time
+Start-Service w32time
