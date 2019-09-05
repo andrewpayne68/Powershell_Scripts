f6f7cbb3c5a8c48403505c995db229b3049dd36d
Twikki <46316044+Twikki@users.noreply.github.com>
2019-09-05 15:43:33 +0200

    Add files via upload
    - Installs NPS on Windows
    - Downloads: Cisco_Radius_Export.xml
    - Imports: Cisco_Radius_Export.xml

---
+++ NPS_Radius_Server_Installation.ps1
@@ -0,0 +1,20 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Installs the Network Policy Server ( Radius Server )
+Install-WindowsFeature NPAS -IncludeManagementTools
+
+
+mkdir c:\tempdir
+
+# Downloads the Radius configuration file from Github
+$Script = Invoke-WebRequest https://raw.githubusercontent.com/Twikki/Powershell_Scripts/master/Cisco_Radius_Export.xml
+
+
+# Creates the file
+Set-Content -Path 'C:\tempdir\Cisco_Radius_Export.xml' -Value $script
+
+
+# Imports the configuration
+Import-NpsConfiguration -Path "C:\Cisco_Radius_Export.xml"
+
+Remove-Item c:\Tempdir -Force -Recurse
