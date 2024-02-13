23214df0d522a994a38622fee669d35fc14aae74
Andrew P <40119314+andrewpayne68@users.noreply.github.com>
2024-02-13 08:09:42 +0000

    Update RunPowershell.cmd

--- RunPowershell.cmd
+++ RunPowershell.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
 SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath=C:\Scripts\Build_Domain.ps1
+SET PowerShellScriptPath=C:\Scripts\Automated_Windows_Updates.ps1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'";
