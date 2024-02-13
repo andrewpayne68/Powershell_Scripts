72bc4d32357a26dfed4d3a8e1db87e8700587df2
Andrew P <40119314+andrewpayne68@users.noreply.github.com>
2024-02-13 11:28:06 +0000

    Update RunPowershell.cmd

--- RunPowershell.cmd
+++ RunPowershell.cmd
@@ -1,3 +1,11 @@
 @ECHO OFF
+if "%1" == "elevated" goto start
+powershell -command "Start-Process %~nx0 elevated -Verb runas"
+goto :EOF
+
+:start
 SET PowerShellScriptPath=C:\SCRIPTS\Automated_Windows_Updates.ps1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'";
+
+:EOF
+exit
